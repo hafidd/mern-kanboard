c60cf58d0d8a3267ad875a1a948bda724eceb919
Hafid <hafid.f.septiawan@gmail.com>
2022-06-14 21:07:38 +0700

    fix

---
+++ build/precache-manifest.d9129e8adcdad11f97e32fff95ec8b47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e01ecef5f50d4b42035eeea18669109",
+    "url": "/index.html"
+  },
+  {
+    "revision": "649078632ecb759d2690",
+    "url": "/static/css/2.2a60accc.chunk.css"
+  },
+  {
+    "revision": "bf319c9121c9dc0be7c3",
+    "url": "/static/css/main.8cfbfc35.chunk.css"
+  },
+  {
+    "revision": "649078632ecb759d2690",
+    "url": "/static/js/2.2ec8cb47.chunk.js"
+  },
+  {
+    "revision": "8a408ea33477ff3fb2d59e207e6dc527",
+    "url": "/static/js/2.2ec8cb47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf319c9121c9dc0be7c3",
+    "url": "/static/js/main.352d8193.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.352d8193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808ef5e9a905f2d37d82",
+    "url": "/static/js/runtime-main.5b21e5df.js"
+  }
+]);
